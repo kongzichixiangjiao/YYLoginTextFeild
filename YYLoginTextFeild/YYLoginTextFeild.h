e65de0e810d6852dd1832aac3810823952349c25
kongzichixiangjiao <58895347@qq.com>
2018-08-04 11:17:41 +0800

    0.0.1

---
+++ YYLoginTextFeild/YYLoginTextFeild.h
@@ -0,0 +1,19 @@
+//
+//  YYLoginTextFeild.h
+//  YYLoginTextFeild
+//
+//  Created by houjianan on 2018/8/3.
+//  Copyright © 2018年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YYLoginTextFeild.
+FOUNDATION_EXPORT double YYLoginTextFeildVersionNumber;
+
+//! Project version string for YYLoginTextFeild.
+FOUNDATION_EXPORT const unsigned char YYLoginTextFeildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYLoginTextFeild/PublicHeader.h>
+
+
